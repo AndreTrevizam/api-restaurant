ae36b26cc2fd3cde21c33445d3b27435864f52ca
AndreTrevizam <andretrevizam264@gmail.com>
2025-03-08 15:13:09 -0300

    initialize project structure with Express, TypeScript, and error handling

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,8 @@
+import { AppError } from "@/utils/AppError";
+import { Request, Response, NextFunction } from "express";
+
+export function errorHandling(error:any, req:Request, res:Response, _:NextFunction) {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ message: error.message })
+  }
+}
