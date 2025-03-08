90c9276f7083b8aafbf2e0d913a9aa4c1e4079f9
AndreTrevizam <andretrevizam264@gmail.com>
2025-03-08 17:54:46 -0300

    add product creation endpoint with validation and error handling

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,14 +1,33 @@
 import { NextFunction, Request, Response } from "express"
+import { knex } from "@/database/knex"
+import { z } from "zod"
 
 class ProductController {
-  async index(req:Request, res:Response, next:NextFunction) {
+  async index(req: Request, res: Response, next: NextFunction) {
     try {
-      
-      return res.json({message: "OK"})
+
+      return res.json({ message: "OK" })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0)
+      })
+
+      const { name, price } = bodySchema.parse(req.body)
+
+      await knex<ProductRepository>("products").insert({name, price})
+
+      return res.status(201).json()
     } catch (error) {
       next(error)
     }
   }
 }
 
-export {ProductController}
+export { ProductController }
