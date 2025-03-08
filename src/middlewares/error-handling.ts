dc44d82b49ac3cdfdba164c5b67af1d6d7a3402e
AndreTrevizam <andretrevizam264@gmail.com>
2025-03-08 15:15:39 -0300

    add error handling middleware to server

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -5,4 +5,6 @@ export function errorHandling(error:any, req:Request, res:Response, _:NextFuncti
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({ message: error.message })
   }
+
+  return res.status(500).json({ message: error.message })
 }
