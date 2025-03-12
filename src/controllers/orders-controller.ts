9591f05f27085d511fc71a926b6d5321d6ec5f55
AndreTrevizam <andretrevizam264@gmail.com>
2025-03-12 19:06:26 -0300

    add orders controller and routes for managing orders

---
+++ src/controllers/orders-controller.ts
@@ -0,0 +1,75 @@
+import { NextFunction, Request, Response } from "express"
+import { knex } from "@/database/knex"
+import { z } from "zod"
+import { AppError } from "@/utils/AppError"
+
+class OrdersController {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        table_session_id: z.number(),
+        product_id: z.number(),
+        quantity: z.number()
+      })
+
+      const { product_id, quantity, table_session_id } = bodySchema.parse(req.body)
+
+      const session = await knex<TableSessionRepository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first()
+
+      if (!session) {
+        throw new AppError("Session table not found")
+      }
+
+      if (session.closed_at) {
+        throw new AppError("This table is closed")
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first()
+
+      if (!product) {
+        throw new AppError("Product not found")
+      }
+
+      await knex<OrderRepository>("orders").insert({
+        product_id,
+        table_session_id,
+        quantity,
+        price: product.price
+      })
+
+      return res.status(201).json()
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async index(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { table_session_id } = req.params
+
+      const order = await knex("orders")
+        .select(
+          "orders.id", 
+          "orders.table_session_id", 
+          "orders.product_id", 
+          "products.name",
+          "orders.price",
+          "orders.quantity"
+        )
+        .join("products", "products.id", "orders.product_id")
+        .where({ table_session_id })
+
+
+      return res.json(order)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export { OrdersController }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { productsRoutes } from "./products-routes"
 import { tablesRouter } from "./tables-routes";
 import { tablesSessionsRoutes } from "./tables-sessions-routes";
+import { ordersRoutes } from "./orders-routes";
 
 const routes = Router()
 routes.use("/products", productsRoutes)
 routes.use("/tables", tablesRouter)
 routes.use("/tables_sessions", tablesSessionsRoutes)
+routes.use("/orders", ordersRoutes)
 
 export { routes }
